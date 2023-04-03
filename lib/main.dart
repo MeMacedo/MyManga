9b0c4f5b6656ddf62b9a1fa6633546069100d34c
˜MeMacedo˜ <melissa.macedo@tokstok.com.br>
2023-04-03 00:06:41 -0300

    :recycle: Refatora main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,19 @@
-import 'package:my_manga_app/views/app_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:my_manga_app/constants/common/strings.dart';
+import 'package:my_manga_app/views/splash/splashscreen.dart';
 
 void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      title: Strings.myManga,
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
